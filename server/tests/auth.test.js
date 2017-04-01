16f1b9eecbb187310ddc2654df96d8b72d3c9150
ngocdam <ngocdt@cardano-labo.com>
2017-04-01 14:44:15 +0700

    ^ change test scripts

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,87 +1,18 @@
 import request from 'supertest-as-promised';
 import httpStatus from 'http-status';
-import jwt from 'jsonwebtoken';
 import chai, { expect } from 'chai';
 import app from '../../index';
-import config from '../../config/config';
 
 chai.config.includeStack = true;
 
 describe('## Auth APIs', () => {
-  const validUserCredentials = {
-    username: 'react',
-    password: 'express'
-  };
-
-  const invalidUserCredentials = {
-    username: 'react',
-    password: 'IDontKnow'
-  };
-
-  let jwtToken;
-
-  describe('# POST /api/auth/login', () => {
-    it('should return Authentication error', (done) => {
+  describe('# GET /api/health-check', () => {
+    it('should return OK', (done) => {
       request(app)
-        .post('/api/auth/login')
-        .send(invalidUserCredentials)
-        .expect(httpStatus.UNAUTHORIZED)
-        .then((res) => {
-          expect(res.body.error.message).to.equal('Authentication error');
-          done();
-        })
-        .catch(done);
-    });
-
-    it('should get valid JWT token', (done) => {
-      request(app)
-        .post('/api/auth/login')
-        .send(validUserCredentials)
+        .get('/api/health-check')
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.result).to.have.property('token');
-          jwt.verify(res.body.result.token, config.jwtSecret, (err, decoded) => {
-            expect(err).to.not.be.ok; // eslint-disable-line no-unused-expressions
-            expect(decoded.username).to.equal(validUserCredentials.username);
-            jwtToken = `Bearer ${res.body.result.token}`;
-            done();
-          });
-        })
-        .catch(done);
-    });
-  });
-
-  describe('# GET /api/auth/random-number', () => {
-    it('should fail to get random number because of missing Authorization', (done) => {
-      request(app)
-        .get('/api/auth/random-number')
-        .expect(httpStatus.UNAUTHORIZED)
-        .then((res) => {
-          expect(res.body.error.message).to.equal('Unauthorized');
-          done();
-        })
-        .catch(done);
-    });
-
-    it('should fail to get random number because of wrong token', (done) => {
-      request(app)
-        .get('/api/auth/random-number')
-        .set('Authorization', 'Bearer inValidToken')
-        .expect(httpStatus.UNAUTHORIZED)
-        .then((res) => {
-          expect(res.body.error.message).to.equal('Unauthorized');
-          done();
-        })
-        .catch(done);
-    });
-
-    it('should get a random number', (done) => {
-      request(app)
-        .get('/api/auth/random-number')
-        .set('Authorization', jwtToken)
-        .expect(httpStatus.OK)
-        .then((res) => {
-          expect(res.body.result.num).to.be.a('number');
+          expect(res.text).to.equal('OK');
           done();
         })
         .catch(done);
