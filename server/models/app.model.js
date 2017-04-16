1a6ff31b0b601d739c0c5cfa0f333a0776f77e18
ngocdam <ngocdt@cardano-labo.com>
2017-04-16 13:20:44 +0700

    - remove all on master, prepare for production

--- server/models/app.model.js
+++
@@ -1,5 +0,0 @@
-// In case the base source support more than one driver,
-// such as postres and mysql,
-// this should base one config to import correct driver.
-
-module.exports = require('./app.postgres.model'); // eslint-disable-line global-require
