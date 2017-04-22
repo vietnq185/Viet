c49902593259a0c2c7626fa7336f2f161cd0ba19
Viet Nguyen <quangviet185@gmail.com>
2017-04-22 19:38:04 +0700

    add banner

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,57 @@
 import React from 'react'
-// import { IndexLink, Link } from 'react-router'
+import { IndexLink, Link } from 'react-router'
+import { fadeIn } from 'react-animations'
+import { StyleSheet, css } from 'aphrodite'
 import './Footer.scss'
+import LeftBGSpecialOffer from '../../styles/images/left-bg-special-offer.png'
 
-export const Footer = () => (
-  <div className='page-footer text-left'>
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-md-6 col-xs-12 col-md-push-6 footer-follow'>
-          {/* <span>
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true
+    }
+  }
+  render() {
+    return (
+      <div className='page-footer text-left'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-md-6 col-xs-12 col-md-push-6 footer-follow'>
+              {/* <span>
             Follow us on
                     <i class="fa fa-fw fa-facebook dk-blue" aria-hidden="true"></i>
             <i class="fa fa-fw fa-twitter dk-blue1" aria-hidden="true"></i>
             <i class="fa fa-fw fa-linkedin-square dk-blue2" aria-hidden="true"></i>
           </span> */}
-        </div>
-        <div className='col-md-6 col-xs-12 col-md-pull-6 footer-action'>
-          <div className='footer-group-item'>
-            <div className='footer-item'><a href='javascript: void(0);'>About us</a></div>
-            <div className='footer-item'><a href='javascript: void(0);'>Contact us</a></div>
-            <div className='footer-item'><a href='javascript: void(0);'>Terms of Service</a></div>
-            <div className='footer-item'><a href='javascript: void(0);'>Privacy</a></div>
+            </div>
+            <div className='col-md-6 col-xs-12 col-md-pull-6 footer-action'>
+              <div className='footer-group-item'>
+                <div className='footer-item'><a href='javascript: void(0);'>About us</a></div>
+                <div className='footer-item'><a href='javascript: void(0);'>Contact us</a></div>
+                <div className='footer-item'><a href='javascript: void(0);'>Terms of Service</a></div>
+                <div className='footer-item'><a href='javascript: void(0);'>Privacy</a></div>
+              </div>
+              <div className='footer-copyright'>
+                <span>2016 <i className='fa fa-fw fa-copyright' /> Copyright by A-SLS. All rights Reserved.</span>
+              </div>
+            </div>
           </div>
-          <div className='footer-copyright'>
-            <span>2016 <i className='fa fa-fw fa-copyright' /> Copyright by A-SLS. All rights Reserved.</span>
+          <div className={['banner-discount-container hidden-xs', (this.state.show ? '' : 'hide')].join(' ')}>
+            <nav className='navbar-fixed-bottom'>
+              <div className='banner-discount'>
+                <span className='left-bg-special-offer'><img src={LeftBGSpecialOffer} /></span>
+                <span className='banner-discount-info'>
+                  Discount 20% for the first 200 subscriptions
+								<Link to='/subscribe' className='btn dk-bg-green dk-white ml2'>Subscribe Now</Link>
+                </span>
+                <span><a className='close' onClick={() => this.setState({ show: false })}>&times;</a></span>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
-export default Footer
