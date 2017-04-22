afa03fef29a9de628bdfa6c32c811117e8cbe2e0
Viet Nguyen <quangviet185@gmail.com>
2017-04-22 11:16:28 +0700

    + add trial popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import React from 'react'
+import { fadeIn } from 'react-animations'
+import { StyleSheet, css } from 'aphrodite'
 import { connect } from 'react-redux'
 import { IndexLink, Link } from 'react-router'
 
@@ -9,7 +11,22 @@ import Utils from '../../helpers/utils'
 
 import './Header.scss'
 
+const styles = StyleSheet.create({
+  fadeIn: {
+    animationName: fadeIn,
+    animationDuration: '1s'
+  }
+})
+
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showFreeTrialConfirmStudent: false,
+      showFreeTrialConfirm: false
+    }
+  }
+
   doLogout() {
     const self = this
     setTimeout(function () {
@@ -32,36 +49,77 @@ class Header extends React.Component {
       }
     }
     return (
-      <nav className='navbar navbar-default dk-navbar' role='navigation'>
-        {/* Brand and toggle get grouped for better mobile display */}
-        <div className='navbar-header'>
-          {/* button for mobile display. show menu */}
-          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='.navbar-ex1-collapse' aria-expanded='false'>
-            <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar' />
-            <span className='icon-bar' />
-            <span className='icon-bar' />
-          </button>
-          <IndexLink to='/' className='navbar-brand'>A-SLS</IndexLink>
+      <div>
+        <nav className='navbar navbar-default dk-navbar' role='navigation'>
+          {/* Brand and toggle get grouped for better mobile display */}
+          <div className='navbar-header'>
+            {/* button for mobile display. show menu */}
+            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='.navbar-ex1-collapse' aria-expanded='false'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+            </button>
+            <IndexLink to='/' className='navbar-brand'>A-SLS</IndexLink>
+          </div>
+          {/* Top Menu Items */}
+          <div className='collapse navbar-collapse navbar-ex1-collapse navbar-right dk-white'>
+            <ul className='nav navbar-nav side-nav'>
+              {/* <li>
+            <IndexLink to='/' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Home</IndexLink>
+          </li> */}
+              <li><Link to='/programme' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Our Programme</Link></li>
+              <li><Link to='/student' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>For Student</Link></li>
+              <li><Link to='/parent' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>For Parent</Link></li>
+              <li><a className='side-nav-item dk-white route--item' href='javascript: void(0);' data-toggle='modal' data-target='#modalFreeTrialConfirm' onClick={() => this.setState({ showFreeTrialConfirm: true })}>Free Trial</a></li>
+              {/* <li><Link to='/subscribe' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Free Trial</Link></li> */}
+              {/* <li><a href="/contact"> <span class="side-nav-item dk-white">Contact</span></a></li> */}
+              {/* <li><a href='javascript: void(0);' data-toggle='modal' data-target='#loginModal'> <span className='side-nav-item dk-white'>Sign Up</span></a></li> */}
+              {subscriptionLink}
+              {theLink}
+            </ul>
+          </div>
+          {/* /.navbar-collapse */}
+        </nav>
+        {/* <!-- Modal --> */}
+        <div id='modalFreeTrialConfirm' aria-hidden='false' className={['modal fade', this.state.showFreeTrialConfirm ? 'in' : '', css(styles.fadeIn)].join(' ')} role='dialog' style={this.state.showFreeTrialConfirm ? { display: 'block' } : { display: 'none' }}>
+          <div className='modal-dialog'>
+            {/* <!-- Modal content--> */}
+            <div className='modal-content'>
+              <div className='modal-header text-center'>
+                <span className='modalFreeTrialConfirmTitle'>Free Trial:</span>
+                <button type='button' className='close' data-dismiss='modal' onClick={() => this.setState({ showFreeTrialConfirm: false })}>&times;</button>
+              </div>
+              <div className='modal-body'>
+                <p className='text-center'>Are you a student or parent?</p><br />
+                <div className='text-center'>
+                  <a className='btn dk-bg-green dk-white mb5' data-dismiss='modal' data-toggle='modal' data-target='#modalFreeTrialConfirmStudent'  onClick={() => this.setState({ showFreeTrialConfirm: false, showFreeTrialConfirmStudent: true })}>I AM A STUDENT</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a className='btn dk-bg-blue dk-white' href='/subscribe'>I AM A PARENT</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* Top Menu Items */}
-        <div className='collapse navbar-collapse navbar-ex1-collapse navbar-right dk-white'>
-          <ul className='nav navbar-nav side-nav'>
-            {/* <li>
-          <IndexLink to='/' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Home</IndexLink>
-        </li> */}
-            <li><Link to='/programme' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Our Programme</Link></li>
-            <li><Link to='/student' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>For Student</Link></li>
-            <li><Link to='/parent' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>For Parent</Link></li>
-            <li><Link to='/subscribe' className='side-nav-item dk-white route--item' activeClassName='route--active dk-yellow'>Free Trial</Link></li>
-            {/* <li><a href="/contact"> <span class="side-nav-item dk-white">Contact</span></a></li> */}
-            {/* <li><a href='javascript: void(0);' data-toggle='modal' data-target='#loginModal'> <span className='side-nav-item dk-white'>Sign Up</span></a></li> */}
-            {subscriptionLink}
-            {theLink}
-          </ul>
+
+        <div id='modalFreeTrialConfirmStudent' aria-hidden='false' className={['modal fade', this.state.showFreeTrialConfirmStudent ? 'in' : '', css(styles.fadeIn)].join(' ')} role='dialog' style={this.state.showFreeTrialConfirmStudent ? { display: 'block' } : { display: 'none' }}>
+          <div className='modal-dialog'>
+            {/* <!-- Modal content--> */}
+            <div className='modal-content'>
+              <div className='modal-header text-center'>
+                <span className='modalFreeTrialConfirmTitle'>Free Trial:</span>
+                <button type='button' className='close' data-dismiss='modal' onClick={() => this.setState({ showFreeTrialConfirmStudent: false })}>&times;</button>
+              </div>
+              <div className='modal-body'>
+                <p className='text-center'>Thank you for your interest! Please let your parents to create an account and signup ASLS for you!</p><br />
+                <div className='text-center'>
+                  <a className='btn dk-bg-green dk-white' href='/subscribe'>PARENT? SIGNUP FOR A-SLS</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* /.navbar-collapse */}
-      </nav>
+
+      </div>
     )
   }
 }
