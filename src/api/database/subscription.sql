4e090cc15b7cfe9a00dee133793c34af74f96653
Viet Nguyen <quangviet185@gmail.com>
2017-05-11 22:56:04 +0700

    In Select plan > The section for discount should be the same as promotion banner.

--- src/api/database/subscription.sql
+++ src/api/database/subscription.sql
@@ -222,6 +222,6 @@ WHERE _id::uuid='55148ffa-6ca0-4930-bb09-cd2a1158d655';
 
 
 INSERT INTO "options" ("foreign_id", "key", "tab_id", "value", "label", "description", "type", "order", "is_visible", "style", "is_public") VALUES
-(1, 'o_remaining_discount_subscription', 1, 200, NULL, 'Remaining discount subscription', 'float', 4, 2, NULL, 'F');
+(1, 'o_remaining_discount_subscription', 1, 200, NULL, 'Remaining discount subscription', 'float', 4, 2, NULL, 'T');
 
 INSERT INTO users (_id, email, role, salt, "firstName", "lastName", phone, "profilePicture", "coverPicture", "userQuote", "hashedPassword", "userToken", provider, facebook, "facebookId", "facebookToken", google, "googleId", "googleToken", username, status, "lastLoginDate", "lastActivityDate", metadata, "dateCreated", "linkCode") VALUES ('c5ec6dfc-6aaf-4a51-970e-1031ae56a930', 'admin@admin.com', 'admin', 'v9gx/S9pLff95exkQS8rzQ==', 'Super', 'Administrator', '84909091101', '', '', '', 'VeiPU5lNvEjfwOIQl6PqwnxWaqORcIQLyc1CnVeXcLuBhCnPxpYQketTG+FGuRR+qCINCXJ8WlBcZ2/lybD7FA==', 'b6af34ad5be7c7198972aa189833dca7e0d6e00c3e16d24e3f0949fc64dba904', 'local', NULL, NULL, NULL, NULL, NULL, NULL, 'c5ec6dfc-6aaf-4a51-970e-1031ae56a930', '{parent}', NULL, NULL, NULL, 1493637306162, 546);
